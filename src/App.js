274914bbee7084e43c2d97b9d8b5ebd47e556574
Evewwal12 <wxy1012candoit@163.com>
2025-04-22 11:35:14 -0700

    using the scope map token

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1>Hello! My name is Eve Wang - testing! 👋</h1>
+      <h1>Hello! My name is Eve Wang - using the scope map token! 👋</h1>
     </div>
   );
 }
